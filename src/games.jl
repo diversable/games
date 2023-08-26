c396df5a1c4688df1e1956355918c44a2bee1be6
Diversable <dan.mantei@outlook.com>
2023-08-26 15:43:54 -0700

    scissors is buggy

--- src/games.jl
+++ src/games.jl
@@ -1,13 +1,14 @@
 module games
 
 println("Which game do you want to play? \nPress 1 for Mad Libs, \n2 for Guess the Number, \n3 for the computer number guessing game, \n4 for rock paper scissors, \nor press 0 to exit")
-choice = readline()
+choice = parse(Int64, readline())
 
 include("mad_libs.jl")
 include("guess_the_number.jl")
 include("computer_guesses_number.jl")
 include("rock_paper_scissors.jl")
 
+# mad_libs.play_mad_libs()
 if choice == 1
     mad_libs.play_mad_libs()
 elseif choice == 2
